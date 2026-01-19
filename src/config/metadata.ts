625559e88b6c804f2995c9db80dfa34e63524338
Leonel César <leonelcesar62rgmail.com>
2026-01-19 12:52:22 +0100

    Pagina inicial do flow invoice

---
+++ src/config/metadata.ts
@@ -0,0 +1,6 @@
+export const APP_METADATA = {
+  APP_NAME: "Flow Invoice",
+  TITLE: "LC-Faturas",
+  DESCRIPTION:
+    "Plataforma moderna para gestão, emissão e controle de faturas.",
+} as const;
